531af7405bb37387b58bbdf25d7ae228a205dd5d
juansanchez <juan_forever95@outlook.com>
2023-08-23 17:09:55 +0200

    udapted

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import moment from 'moment';
 
+import PortfolioContainer from "./portfolio-container";
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>DevCamp React Starter</h1>
         <h2>React Redux Router</h2>
+        <PortfolioContainer />
         <div>
           {moment().format('MMMM Do YYYY, h:mm:ss a')}
         </div>
